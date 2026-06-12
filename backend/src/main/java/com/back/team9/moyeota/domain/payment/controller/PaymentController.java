c210429d1dc1c1aeb6b6479a97e6cdb9ecc19e04
jiyeongxxxkim <wldud128iop@gmail.com>
2026-06-12 09:48:29 +0900

    feat: 토스페이먼츠 보증금(DEPOSIT) 결제 승인 API 구현 Closes #1 (#8)
    * feat: 토스페이먼츠 보증금 결제 승인 API 구현 및 단위 테스트 추가
    
    * refactor: Gemini 코드 리뷰 반영

--- backend/src/main/java/com/back/team9/moyeota/domain/payment/controller/PaymentController.java
+++ backend/src/main/java/com/back/team9/moyeota/domain/payment/controller/PaymentController.java
@@ -1,4 +1,29 @@
 package com.back.team9.moyeota.domain.payment.controller;
 
+import com.back.team9.moyeota.domain.payment.dto.PaymentConfirmRequest;
+import com.back.team9.moyeota.domain.payment.dto.PaymentResponse;
+import com.back.team9.moyeota.domain.payment.service.PaymentService;
+import com.back.team9.moyeota.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/deposit/confirm")
+    public ResponseEntity<ApiResponse<PaymentResponse>> confirmDeposit(
+            @RequestBody @Valid PaymentConfirmRequest request
+            ) {
+        PaymentResponse response = paymentService.confirmDeposit(request);
+        return ResponseEntity.ok(new ApiResponse<>("SUCCESS","보증금 결제가 완료되었습니다.", response));
+    }
 }
--- backend/src/main/java/com/back/team9/moyeota/domain/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/back/team9/moyeota/domain/payment/repository/PaymentRepository.java
@@ -3,5 +3,10 @@ package com.back.team9.moyeota.domain.payment.repository;
 import com.back.team9.moyeota.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(String orderId);
+    Optional<Payment> findByTossPaymentKey(String tossPaymentKey);
+
 }
