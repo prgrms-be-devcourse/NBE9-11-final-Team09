de0eedaa13d9ab855b002728d023ba3911f79e5f
karo119 <jsin991019@gmail.com>
2026-06-22 12:46:13 +0900

    chorefix: 누락 import 추가

--- backend/src/main/java/com/back/team9/moyeota/global/config/SecurityConfig.java
+++ backend/src/main/java/com/back/team9/moyeota/global/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.back.team9.moyeota.global.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
