f557df318991c1ccd216aba301eb0f317915d1dc
jiyeongxxxkim <wldud128iop@gmail.com>
2026-06-22 12:36:04 +0900

    feat: Admin 정산 페이백 승인/거절 ADMIN 권한 검증 적용 (#120)

--- backend/src/main/java/com/back/team9/moyeota/global/config/SecurityConfig.java
+++ backend/src/main/java/com/back/team9/moyeota/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.back.team9.moyeota.global.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
