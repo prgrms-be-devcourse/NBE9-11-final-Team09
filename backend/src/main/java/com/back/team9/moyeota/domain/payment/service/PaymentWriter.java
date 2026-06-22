fc2e7f07d830cbfd52f027612b8bb980e6ee6c82
suuuu9 <asb040923@gmail.com>
2026-06-22 15:17:15 +0900

    feat: PaymentWriter에 saveRefundStatus() REQUIRES_NEW 메서드 추가 (#125)

--- backend/src/main/java/com/back/team9/moyeota/domain/payment/service/PaymentWriter.java
+++ backend/src/main/java/com/back/team9/moyeota/domain/payment/service/PaymentWriter.java
@@ -5,6 +5,7 @@ import com.back.team9.moyeota.domain.payment.entity.PaymentStatus;
 import com.back.team9.moyeota.domain.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
@@ -21,6 +22,11 @@ public class PaymentWriter {
     @Transactional
     public Payment update(Payment payment, PaymentStatus status) {
         payment.updateStatus(status);
+        return paymentRepository.save(payment);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Payment saveRefundStatus(Payment payment) {
         return paymentRepository.save(payment);
     }
 }
