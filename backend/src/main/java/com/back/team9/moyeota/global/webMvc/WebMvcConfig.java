d1c86f92da3c7d6d33b0c71f5d419395bc514743
gmltmdu <dbtmdgml03@naver.com>
2026-06-26 10:21:36 +0900

    fix: prod, dev 다르게 주입

--- backend/src/main/java/com/back/team9/moyeota/global/webMvc/WebMvcConfig.java
+++ backend/src/main/java/com/back/team9/moyeota/global/webMvc/WebMvcConfig.java
@@ -1,15 +1,22 @@
 package com.back.team9.moyeota.global.webMvc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
